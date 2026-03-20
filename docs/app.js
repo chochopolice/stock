28f23d85f8f2696a56ac006e82b652b1ba5ce147
chochopolice <o06d012@yahoo.co.jp>
2026-03-21 00:37:10 +0900

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -282,11 +282,11 @@ async function init() {
 // ============================
 $btnAnalyze.disabled = true;
 $btnResolve.addEventListener("click", () => {
-  $btnAnalyze.disabled = false;
   const q = $input.value;
   const { resolved: r, candidates } = resolveTicker(q);
   renderCandidates(candidates);
   if (r) {
+    $btnAnalyze.disabled = false;
     // resolved = { code: r.code, name: r.name };
    // $resolvedText.textContent = `${r.code} ${r.name}`; //候補が1でもユーザに選択させる
     setResult("(候補を表示しました。必要なら候補をクリックして変更できます)");
